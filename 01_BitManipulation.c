4cfa417bbfe1a9e03569f372167d7361c85849ba
anoojpraba <anooj.prabaharan@outlook.com>
2021-12-21 08:21:57 -0800

    Added Bit manipulation as Function

--- 01_BitManipulation.c
+++ 01_BitManipulation.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 /*
- *
- *
  *  ip-> 0xDCBA
  *  op -> 0xBACD
- *
  */
-int main(void)
+void Function1()
 {
     int ip=0xdcba;
     int op=((ip<<8 & 0xFF00)|(ip>>12)|(ip>>4 &0xf0));
     printf("0x%4x\n",op);
+}
+int main(void)
+{
+    Function1();
     return 0;
 }
