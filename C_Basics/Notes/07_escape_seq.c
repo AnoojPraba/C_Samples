f724b6bffa73010fecc6ea48280b52046e1c3dcb
anoojpraba <anooj.prabaharan@outlook.com>
2022-01-02 18:19:59 -0800

    added escape sequence

---
+++ C_Basics/Notes/07_escape_seq.c
@@ -0,0 +1,17 @@
+//Escape sequence that is in c are as follows
+//
+
+\a  -   Alarm or Beep
+\b  -   Backspace
+\f  -   Formfeed
+\n  -   New Line
+\r  -   Carrage return
+\t  -   Tan
+\v  -   Vertical Tan
+\\  -   Backslash
+\'  -   Single Quotes
+\"  - Double quotes
+\?  -   Question Mark
+\ooo-   Octal
+\xhh-   Hex
+\0  -   Null
