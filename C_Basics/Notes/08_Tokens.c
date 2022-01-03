bb36b8940c81b58ea04f9362b5f58bcd001260e9
anoojpraba <anooj.prabaharan@outlook.com>
2022-01-03 10:48:14 -0800

    added tokens operation

---
+++ C_Basics/Notes/08_Tokens.c
@@ -0,0 +1,37 @@
+//Classifications
+*   Keywords
+        preedefined or reserved words for compiler. Hence cannot redefine the same
+        //Keywords in c
+        auto         double      int        struct
+        break        else        long       switch
+        case         enum        register   typedef
+        char         extern      return     union
+        const        float       short      unsigned
+        continue     for         signed     void
+        default      goto        sizeof     volatile
+        do           if          static     while 
+        //Keywords in cpp
+        asm          bool        catch          class
+        const_cast   delete      dynamic_cast   explicit 
+        export       false       friend         inline 
+        mutable      namespace   new            operator 
+        private      protected   public         reinterpret_cast
+        static_cast  template    this           throw
+        true         try         typeid         typename 
+        using        virtual     wchar_t 
+*   identifiers
+        terminology for naming of variable, function and array.
+        letter and underscroll can be first letter in naming convension.
+        It should be up to 31 characters long as only the first 31 characters are significant.
+*   constant or litrals
+        Their values can not be modified by the program once they are defined. Constants refer to fixed values
+*   strings
+*   special symbols
+        following symbols cannot be used for some other purpose.[] () {}, ; * = # as it has some other purpose
+*   operators
+        Symbols that trigger action while applied on c variables.
+            Three broad classification
+                *   Unary Operator (increment or decrement)
+                *   Binary Operator (arithmtic and bit wise)
+                *   trinary operator (condition?true:false)
+
